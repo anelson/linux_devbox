e0c726945565e621aafb8ac9ac45045efe35d9f2
Unknown <anelson@apocryph.org>
2017-04-08 21:41:03 -0400

    Fix vim tests to properly verify pip2 package installation
    The pip resource in InSpec uses the Python 3 version so it fails

--- test/smoke/default/compile_vim.rb
+++ test/smoke/default/compile_vim.rb
@@ -29,10 +29,10 @@ describe file '/usr/share/applications/vim.desktop' do
 	it { should be_readable }
 end
 
-describe pip('sexpdata') do 
-	it { should be_installed }
+describe command('pip2 show sexpdata') do 
+	its('exit_status') { should eq 0 }
 end
 
-describe pip('websocket-client') do 
-	it { should be_installed }
+describe command('pip2 show websocket-client') do 
+	its('exit_status') { should eq 0 }
 end
