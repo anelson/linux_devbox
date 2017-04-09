c71ead16a0d40261e985700a28724e6457a8e885
Unknown <anelson@apocryph.org>
2017-04-08 21:41:28 -0400

    Explicitly set the IDEA URL since the idea cookbook is too stupid

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,13 @@
 default['java']['install_flavor'] = 'openjdk'
 default['java']['jdk_version'] = '8'
 
+edition = 'U'
+version = '2017.1'
 default['idea']['setup_dir'] = '/opt/ideaIU'
-default['idea']['edition'] = 'U'
-default['idea']['version'] = '2017.1'
+default['idea']['edition'] = edition
+default['idea']['version'] = version
+# the idea cookbook is implemented in such a way that changing edition and version doesn't update the URL
+default['idea']['url'] = "https://download-cf.jetbrains.com/idea/ideaI#{edition}-#{version}.tar.gz"
 
 default['linux_devbox']['user'] = ENV['SUDO_USER'] || node[:current_user]
 default['linux_devbox']['vim_tarball'] = ["ftp://ftp.vim.org/pub/vim/unix/vim-8.0.tar.bz2", "ftp://ftp.ca.vim.org/pub/vim/unix/vim-8.0.tar.bz2"]
