96b199bcdec7ba92b7bf15d3042a608920bd59f2
Adam Nelson <anelson@apocryph.org>
2018-02-01 18:38:49 -0500

    Set GTK and QT env vars for HiDPI support in XPS configs

---
+++ playbooks/roles/xps/files/gtk-hidpi-env-vars.sh
@@ -0,0 +1,8 @@
+# Set environment variables telling GTK apps
+# to use HiDPI on XPS systems
+# disabled for now due to ridiculous results
+export GDK_SCALE=2
+
+# Tell QT5 apps also to auto detect hidpi
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+
